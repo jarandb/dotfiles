1d044e73bd61f79454298d039bbe2b08c09bdc03
Jarand Bjerkhaug <jarand.bjerkhaug@esmartsystems.com>
2024-11-25 13:51:09 +0100

    added sketchybarrc back

--- nvim/lua/jarand/plugins/oil.lua
+++ nvim/lua/jarand/plugins/oil.lua
@@ -1,30 +1,30 @@
 return {
-	-- 	"stevearc/oil.nvim",
-	-- 	---@module 'oil'
-	-- 	---@type oil.SetupOpts
-	-- 	opts = {},
-	-- 	-- Optional dependencies
-	-- 	dependencies = { { "echasnovski/mini.icons", opts = {} } },
-	-- 	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-	-- 	config = function()
-	-- 		require("oil").setup({
-	--
-	-- 			default_file_explorer = true,
-	-- 			skip_confirm_for_simple_edits = false,
-	-- 			use_default_keymaps = true,
-	-- 			view_options = {
-	-- 				-- Show files and directories that start with "."
-	-- 				show_hidden = true,
-	-- 			},
-	--
-	-- 			-- keymaps
-	-- 			vim.api.nvim_set_keymap("n", "<leader>o", ":Oil<CR>", { desc = "Open oil", noremap = true, silent = true }),
-	-- 			vim.api.nvim_set_keymap(
-	-- 				"n",
-	-- 				"<leader>fo",
-	-- 				":Oil --float <CR>",
-	-- 				{ desc = "Open oil in floating window", noremap = true, silent = true }
-	-- 			),
-	-- 		})
-	-- 	end,
+	"stevearc/oil.nvim",
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {},
+	-- Optional dependencies
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+	config = function()
+		require("oil").setup({
+
+			default_file_explorer = true,
+			skip_confirm_for_simple_edits = false,
+			use_default_keymaps = true,
+			view_options = {
+				-- Show files and directories that start with "."
+				show_hidden = true,
+			},
+
+			-- keymaps
+			vim.api.nvim_set_keymap("n", "<leader>o", ":Oil<CR>", { desc = "Open oil", noremap = true, silent = true }),
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>fo",
+				":Oil --float <CR>",
+				{ desc = "Open oil in floating window", noremap = true, silent = true }
+			),
+		})
+	end,
 }
